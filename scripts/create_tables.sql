0c430ce10ada1fdebfd5c7982f7a4cf43cfa2c3e
rfzanetti <rfzanetti@gmail.com>
2020-04-03 00:37:45 -0300

    created first version of sql scripts

---
+++ scripts/create_tables.sql
@@ -0,0 +1,22 @@
+CREATE TABLE currency (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+                       code VARCHAR(3) NOT NULL, 
+                       symbol VARCHAR(5), 
+                       usd_conversion double);
+
+CREATE TABLE country (id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+                      name VARCHAR(25) NOT NULL,
+                      eshop_url VARCHAR(100) NOT NULL, 
+                      currency_id INT,
+                      FOREIGN KEY (currency_id) REFERENCES currency(id));
+                      
+CREATE TABLE game (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+                   name VARCHAR(100) NOT NULL);
+
+CREATE TABLE price (id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+                    name VARCHAR(25) NOT NULL,
+                    price DOUBLE, 
+                    timestamp BIGINT,
+                    game_id INT,
+                    FOREIGN KEY (game_id) REFERENCES game(id),
+                    country_id INT,
+                    FOREIGN KEY (country_id) REFERENCES country(id));
