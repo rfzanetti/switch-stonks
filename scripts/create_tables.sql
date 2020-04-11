e38e11b12c3660e4793888a2b097791bf0e3d757
rfzanetti <rfzanetti@gmail.com>
2020-04-11 02:40:29 -0300

    adapting scripts for sqlite

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,17 +1,22 @@
-CREATE TABLE country (id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, 
-                      name VARCHAR(25) NOT NULL,
-                      eshop_url VARCHAR(100) NOT NULL, 
-                      currency_code VARCHAR(3) NOT NULL, 
-                      currency_symbol VARCHAR(5), 
-                      currency_usd_conversion double);
+CREATE TABLE country (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(25) NOT NULL,
+    eshop_url VARCHAR(100) NOT NULL, 
+    currency_code VARCHAR(3) NOT NULL, 
+    currency_symbol VARCHAR(5), 
+    currency_usd_conversion REAL
+);
                       
-CREATE TABLE game (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
-                   name VARCHAR(100) NOT NULL);
+CREATE TABLE game (
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(100) NOT NULL
+);
 
-CREATE TABLE listing (id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT, 
-                      value DOUBLE, 
-                      date DATE,
-                      game_id INT,
-                      FOREIGN KEY (game_id) REFERENCES game(id),
-                      country_id INT,
-                      FOREIGN KEY (country_id) REFERENCES country(id));
+CREATE TABLE listing (
+    listing_value REAL, 
+    listing_date DATE,
+    game_id INTEGER NOT NULL,
+    country_id INTEGER NOT NULL,
+    FOREIGN KEY (game_id) REFERENCES game (id),
+    FOREIGN KEY (country_id) REFERENCES country (id)
+);
--- scripts/insert_countries.sql
+++ scripts/insert_countries.sql
@@ -1,7 +1,7 @@
-INSERT INTO country values(1, "Brazil", "https://store.nintendo.com.br/", "BRL", "R$", 5.26);
-INSERT INTO country values(2, "Argentina", "https://store.nintendo.com.ar/", "ARS", "$", 64.59);
-INSERT INTO country values(3, "Chile", "https://store.nintendo.cl/", "CLP", "$", 964);
-INSERT INTO country values(4, "Peru", "https://store.nintendo.com.pe/", "PEN", "S/.", 3.49);
-INSERT INTO country values(5, "Colombia", "https://store.nintendo.co/", "COP", "$", 4040.01);
+INSERT INTO country (name, eshop_url, currency_code, currency_symbol, currency_usd_conversion) values("Brazil", "https://store.nintendo.com.br/", "BRL", "R$", 5.26);
+INSERT INTO country (name, eshop_url, currency_code, currency_symbol, currency_usd_conversion) values("Argentina", "https://store.nintendo.com.ar/", "ARS", "$", 64.59);
+INSERT INTO country (name, eshop_url, currency_code, currency_symbol, currency_usd_conversion) values("Chile", "https://store.nintendo.cl/", "CLP", "$", 964);
+INSERT INTO country (name, eshop_url, currency_code, currency_symbol, currency_usd_conversion) values("Peru", "https://store.nintendo.com.pe/", "PEN", "S/.", 3.49);
+INSERT INTO country (name, eshop_url, currency_code, currency_symbol, currency_usd_conversion) values("Colombia", "https://store.nintendo.co/", "COP", "$", 4040.01);
 
     
