da4c9e623d556dc9331b86a68decfe927cb487b7
rfzanetti <rfzanetti@gmail.com>
2020-04-29 23:36:39 -0300

    Eliminating repeated code on prices-manager models

--- prices_manager/model.py
+++ prices_manager/model.py
@@ -1,35 +1,29 @@
 from peewee import Model, SqliteDatabase, CharField, DateField, FloatField, ForeignKeyField
 
-db = SqliteDatabase("../switch_stonks.db")
+
+class BaseModel(Model):
+    class Meta:
+        database = SqliteDatabase("../switch_stonks.db")
 
 
-class Country(Model):
+class Country(BaseModel):
     name = CharField()
     eshop_url = CharField()
     currency_code = CharField()
     currency_symbol = CharField()
     currency_usd_conversion = FloatField()
 
-    class Meta:
-        database = db
 
-
-class Game(Model):
+class Game(BaseModel):
     title = CharField()
     min_price = FloatField()
     min_price_country_id = ForeignKeyField(Country, backref='game')
     last_updated = DateField()
 
-    class Meta:
-        database = db
 
-
-class Listing(Model):
+class Listing(BaseModel):
     original_value = FloatField()
     usd_value = FloatField()
     date = DateField()
     game = ForeignKeyField(Game, backref='listing')
     country = ForeignKeyField(Country, backref='listing')
-
-    class Meta:
-        database = db
